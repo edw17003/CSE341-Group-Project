308ea9e5f4a0a75c95cf83e622cdcff8192128e2
Miguel Condori <miguel.angel.condori@gmail.com>
2023-11-15 18:04:16 -0300

    added art part in swagger and model

---
+++ models/Art.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const ArtSchema = new mongoose.Schema({
+  artId: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  publicationDate: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Art', ArtSchema, 'arts');
+
