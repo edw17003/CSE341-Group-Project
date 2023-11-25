b74d7f039ac02de9ae482f5a18ddfcc618242661
edw17003 <edw17003@byui.edu>
2023-11-25 16:49:39 -0700

    Fixed deleteArt test

--- jest.tex/artController.deleteArt.test.js
+++ jest.tex/artController.deleteArt.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const app = require('../index');
-const Art = require('../models/Books');
+const Art = require('../models/Art');
 
 jest.mock('../models/Art', () => ({
   findOneAndDelete: jest.fn(),  // Mock the findOneAndDelete method
