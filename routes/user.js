3adef0f82cc967a326e6f17b87b61462790326ac
edw17003 <edw17003@byui.edu>
2023-12-07 22:05:50 -0700

    using jwt middleware in user routes

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const routes = require('express').Router();
 const users = require('../controllers/user.js');
+const  checkRoleAuth = require('../middleware/jwtAuth');
 
-routes.get('/', users.findAll);
-routes.get('/:_id', users.findById);
-routes.post('/', users.create);
-routes.put('/:_id', users.editById);
-routes.delete('/:_id', users.deleteById);
+routes.get('/', checkRoleAuth([1]), users.findAll);
+routes.get('/:_id', checkRoleAuth([1]), users.findById);
+routes.post('/', checkRoleAuth([3]), users.create);
+routes.put('/:_id', checkRoleAuth([3]), users.editById);
+routes.delete('/:_id', checkRoleAuth([3]), users.deleteById);
 
 module.exports = routes;
